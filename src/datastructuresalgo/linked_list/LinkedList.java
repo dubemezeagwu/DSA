8a733ed477e6acea328e51bf0c0c3a92ea630534
Dubem Ezeagwu <48288857+dubemezeagwu@users.noreply.github.com>
2023-04-17 15:03:45 -0400

    sample leetcode interview questions for linked list

--- src/datastructuresalgo/linked_list/LinkedList.java
+++ src/datastructuresalgo/linked_list/LinkedList.java
@@ -228,4 +228,23 @@ public class LinkedList {
         length += otherList.length;
     }
 
+    public Node findKthFromEnd(int k){
+        if (k <= 0 || k > length ) return null;
+
+        Node slow = head;
+        Node fast = head;
+
+        for (int i = 0; i < k; i++) {
+            if (fast == null) return null;
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
 }
